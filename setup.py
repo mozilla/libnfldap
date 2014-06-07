c77af5bbdc507ed7be546816343d353c9d5df406
Julien Vehent <julien@linuxwall.info>
2014-06-07 11:58:46 -0400

    pypi fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 setup(
     name = "libnfldap",
 	py_modules=['libnfldap'],
-    version = "1.0.0",
+    version = "1.0.4",
     author = "Julien Vehent",
     author_email = "jvehent@mozilla.com",
     description = ("A client library to generate ipset and iptables rules from LDAP records."),
