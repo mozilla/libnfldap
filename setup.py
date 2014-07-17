345263aa76c352cb7278f4139a1c01ac73707a9a
Julien Vehent <julien@linuxwall.info>
2014-07-17 17:41:07 -0400

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 setup(
     name = "libnfldap",
 	py_modules=['libnfldap'],
-    version = "1.0.4",
+    version = "1.0.5",
     author = "Julien Vehent",
     author_email = "jvehent@mozilla.com",
     description = ("A client library to generate ipset and iptables rules from LDAP records."),
